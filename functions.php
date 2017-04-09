4c8d12c17a02f965965e01f00411d9dd8c7cf167
isabel <isabelcnd@gmail.com>
2017-04-09 00:34:12 -0400

    % Modificado llamdo a la activacion de menus

--- functions.php
+++ functions.php
@@ -7,11 +7,13 @@
 		wp_enqueue_script('js_bootstrap_', get_template_directory_uri() . '/assets/js/bootstrap.min.js', array("jquery_"), '3.2.1', true);
 		wp_enqueue_script('style_js_', get_template_directory_uri() . '/assets/js/styles.js', array(), '1.0.0', true);
 		wp_enqueue_style( "mod_styles", get_template_directory_uri() . '/style.css', array(), '1.0.0', 'all');
+		wp_enqueue_style( "footer_styles", get_template_directory_uri() . '/assets/css/footer.css', array(), '1.0.0', 'all');
 	}
 	add_action( 'wp_enqueue_scripts', 'css_js_scripts' );
 
 	function some_menus_additionals() {
 		add_theme_support( 'menus' );
 		register_nav_menu( 'principal', 'Menu conoceme' );
+		register_nav_menu( 'secundario', 'Menu encuentrame' );
 	}
-	add_action( 'init', 'some_menus_additional', 10, 1 );
+	add_action( 'init', 'some_menus_additionals');
