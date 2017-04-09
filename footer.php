de0cf7a84e97368f3c13bcd8e0506ec7e132039d
isabel <isabelcnd@gmail.com>
2017-04-09 00:33:01 -0400

    + Agregadp info al footer

--- footer.php
+++ footer.php
@@ -1,6 +1,5 @@
 
-<footer>
-  asdasdasd
-  <?php wp_footer();?>
+<footer><?php wp_footer();?> 
+  <div class="container"><?php wp_nav_menu(array( 'theme_location' => 'secundario', 'container' => 'nav')) ?></div>
 </footer></body>
 </html>
