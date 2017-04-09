81c7a95f57a77665219f2999de627a699a05977e
isabel <isabelcnd@gmail.com>
2017-04-08 22:32:26 -0400

    + Agregado archivo de funciones javascript

--- functions.php
+++ functions.php
@@ -5,6 +5,13 @@
 		wp_enqueue_style( 'styles-bootstrap', get_template_directory_uri() . '/assets/css/bootstrap.min.css' );
 		wp_enqueue_script('jquery_', get_template_directory_uri() . '/assets/js/jquery.min.js', array(), '3.2.1', true);
 		wp_enqueue_script('js_bootstrap_', get_template_directory_uri() . '/assets/js/bootstrap.min.js', array("jquery_"), '3.2.1', true);
+		wp_enqueue_script('style_js_', get_template_directory_uri() . '/assets/js/styles.js', array(), '1.0.0', true);
 		wp_enqueue_style( "mod_styles", get_template_directory_uri() . '/style.css', array(), '1.0.0', 'all');
 	}
 	add_action( 'wp_enqueue_scripts', 'css_js_scripts' );
+
+	function some_menus_additionals() {
+		add_theme_support( 'menus' );
+		register_nav_menu( 'principal', 'Menu conoceme' );
+	}
+	add_action( 'init', 'some_menus_additional', 10, 1 );
