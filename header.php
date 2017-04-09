9fea30f5e96f93df9d8969f1a15a995a78210ea1
isabel <isabelcnd@gmail.com>
2017-04-09 15:38:16 -0400

    % Modificado los estilos del header

--- header.php
+++ header.php
@@ -5,7 +5,8 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Some wordpress web page test</title>
   </head>
-  <body>
+  <body class="container">
     <header>
       <div class="container"><?php wp_nav_menu(array( 'theme_location' => 'principal', 'menu_class' => 'menu-flotante', 'container' => 'nav')) ?></div>
     </header>
+  
