b9451aa5cfbae4239113c30b08b7fab54cf1fe5a
isabel <isabelcnd@gmail.com>
2017-04-24 00:48:52 +0000

    modificado nombre del estilo del main

--- functions.php
+++ functions.php
@@ -5,8 +5,9 @@
 		wp_enqueue_style( 'styles-bootstrap', get_template_directory_uri() . '/assets/css/bootstrap.min.css' );
 		wp_enqueue_script('jquery_', get_template_directory_uri() . '/assets/js/jquery.min.js', array(), '3.2.1', true);
 		wp_enqueue_script('js_bootstrap_', get_template_directory_uri() . '/assets/js/bootstrap.min.js', array("jquery_"), '3.2.1', true);
+		// Estilos personalizados
 		wp_enqueue_script('style_js_', get_template_directory_uri() . '/assets/js/styles.js', array(), '1.0.0', true);
-		wp_enqueue_style( "mod_styles", get_template_directory_uri() . '/style.css', array(), '1.0.0', 'all');
+		wp_enqueue_style( "index_styles", get_template_directory_uri() . '/style.css', array(), '1.0.0', 'all');
 		wp_enqueue_style( "footer_styles", get_template_directory_uri() . '/assets/css/footer.css', array(), '1.0.0', 'all');
 		wp_enqueue_style( "section_tuts_styles", get_template_directory_uri() . '/assets/css/sectionTuts.css', array(), '1.0.0', 'all');
 		wp_enqueue_style( "main_articles_styles", get_template_directory_uri() . '/assets/css/main.css', array(), '1.0.0', 'all');
